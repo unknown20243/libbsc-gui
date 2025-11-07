4c47754753e889c1324eb2bd1dc76a4498bd25bd
unknown20243 <mahboobkjali@gmail.com>
2025-11-07 13:07:19 +0500

    Refactor main.cpp to use MainWindow class

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
 #include <QApplication>
-#include <QPushButton>
-#include <QWidget>
-#include <QFileDialog>
-#include <QProcess>
-#include <QMessageBox>
-#include <QHBoxLayout>
-#include <QLabel>
-#include <QComboBox>
-#include <QLineEdit>
-#include <QCheckBox>
-#include <QGroupBox>
-#include <QTextEdit>
+#include <QPalette>
 #include <QStyleFactory>
+#include "gui.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -30,45 +20,7 @@ int main(int argc, char *argv[]) {
     darkPalette.setColor(QPalette::BrightText, Qt::red);
     app.setPalette(darkPalette);
 
-    QWidget window;
-    window.setWindowTitle("libbsc GUI");
-
-    auto *layout = new QVBoxLayout(&window);
-
-    QLabel *fileLabel = new QLabel("Select file to compress:");
-    QPushButton *browseButton = new QPushButton("Browse...");
-    QLineEdit *filePath = new QLineEdit();
-
-    QObject::connect(browseButton, &QPushButton::clicked, [&]() {
-        QString file = QFileDialog::getOpenFileName(nullptr, "Select file");
-        if (!file.isEmpty()) filePath->setText(file);
-    });
-
-    QHBoxLayout *fileLayout = new QHBoxLayout();
-    fileLayout->addWidget(filePath);
-    fileLayout->addWidget(browseButton);
-
-    QPushButton *compressBtn = new QPushButton("Compress to BSC");
-    QObject::connect(compressBtn, &QPushButton::clicked, [&]() {
-        QString input = filePath->text();
-        if (input.isEmpty()) {
-            QMessageBox::warning(nullptr, "Error", "Please select a file first!");
-            return;
-        }
-        QString output = input + ".bsc";
-        QProcess process;
-        process.start("bsc.exe", QStringList() << "e" << input << output);
-        process.waitForFinished(-1);
-        QMessageBox::information(nullptr, "Done", "Compression completed!");
-    });
-
-    layout->addWidget(fileLabel);
-    layout->addLayout(fileLayout);
-    layout->addWidget(compressBtn);
-
-    window.setLayout(layout);
-    window.resize(500, 200);
+    MainWindow window;
     window.show();
-
     return app.exec();
 }
